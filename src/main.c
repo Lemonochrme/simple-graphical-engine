98e1e1756fec40729565a192ec01aa88268a91be
lemonochrme <marin-muller@insa-toulouse.fr>
2025-03-09 23:05:26 +0100

    Implemented simple framebuffer

--- src/main.c
+++ src/main.c
@@ -2,20 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 600
+#define WIN_WIDTH 1280
+#define WIN_HEIGHT 720
 
-void draw_pixel(Display *display, Window window, GC gc, int x, int y) {
-    XDrawPoint(display, window, gc, x, y);
-}
+unsigned int framebuffer[WIN_WIDTH * WIN_HEIGHT];
 
-void draw_line(Display *display, Window window, GC gc, int x1, int y1, int x2, int y2) {
-    XDrawLine(display, window, gc, x1, y1, x2, y2);
-}
-
-void draw_rectangle(Display *display, Window window, GC gc, int x, int y, int width, int height) {
-    XDrawRectangle(display, window, gc, x, y, width, height);
+void put_pixel(int x, int y, unsigned int color) {
+    if (x >= 0 && x < WIN_WIDTH && y>= 0 && y < WIN_HEIGHT) {
+        framebuffer[y * WIN_WIDTH + x] = color;
+    }
 }
 
 int main() {
@@ -40,12 +37,21 @@ int main() {
     GC gc = XCreateGC(display, window, 0, NULL);
     XSetForeground(display, gc, BlackPixel(display, screen));
     
+    // Stocking the framebuffer
+    XImage *image = XCreateImage(display, DefaultVisual(display, screen), DefaultDepth(display, screen), ZPixmap, 0, (char*) framebuffer, WIN_WIDTH, WIN_HEIGHT, 32, 0);
+
+    // fill framebuffer with black background color
+    memset(framebuffer, 000, sizeof(framebuffer)); 
+
+    // draw horizontal line in white
+    for (int i = 0; i < 500; i++) {
+        put_pixel(100 + i, 100 + i, 0xFFFFFF);
+    }
+
     while (1) {
         XNextEvent(display, &event);
         if (event.type == Expose) {
-            draw_pixel(display, window, gc, 400, 300);
-            draw_line(display, window, gc, 100, 100, 700, 500);
-            draw_rectangle(display, window, gc, 200, 200, 400, 200);
+            XPutImage(display, window, gc, image, 0, 0, 0, 0, WIN_WIDTH, WIN_HEIGHT);
         }
         if (event.type == KeyPress)
             break;
